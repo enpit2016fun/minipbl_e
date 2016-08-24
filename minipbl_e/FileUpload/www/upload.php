3712d4c35e1667e959b352080709c3a0183b2a20
Kenya Kobayashi <16043021@mmm.muroran-it.ac.jp>
2016-08-24 10:52:41 +0900

    echo filepath

--- minipbl_e/FileUpload/www/upload.php
+++ minipbl_e/FileUpload/www/upload.php
@@ -10,6 +10,7 @@ if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
   if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "files/" . $_FILES["upfile"]["name"])) {
     chmod("files/" . $_FILES["upfile"]["name"], 0644);
     echo $_FILES["upfile"]["name"] . "をアップロードしました。";
+    echo "ファイルパス："."files/".$_FILES["upfile"]["name"];
   } else {
     echo "ファイルをアップロードできません。";
   }
