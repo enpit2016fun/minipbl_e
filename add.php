f3772e99f554eabad9ca6c890e150adbe1bf22ce
ShioriTerasaki <g2116028@fun.ac.jp>
2016-08-25 19:25:06 +0900

    add.cssを統合

--- add.php
+++ add.php
@@ -15,9 +15,7 @@ if(isset($_POST["add"])){
                 $email = htmlspecialchars($_POST['email']);
 				$graduation_year = $_POST['graduation_year'];
 				$issend = $_POST['issend'];
-                $zip11 = htmlspecialchars($_POST['zip11']);
-				$addr11 = htmlspecialchars($_POST['addr11']);
-				$infoarr = array($email,$p_name,$c_name,$graduation_year,$issend,$zipll,$addrll);
+				$infoarr = array($email,$p_name,$c_name,$graduation_year,$issend);
 				$_SESSION['arr'] = $infoarr;
 				$pdo = null;
 				//ユーザID、パスワードが入力されていなかったらエラーメッセージを表示
@@ -41,20 +39,27 @@ if(isset($_POST["add"])){
 <link rel="STYLESHEET" href="./css/add.css" type="text/css">
 </head>
 <body>
+<form action='add.php' method='POST'>
 
 	<div id="site-box">
 		<div id="head">
-			<h2 align="cenet">新規登録</h2>
+
 		</div>
 		<!-- #head -->
 <div id="form-main">
 <div id="form-div">
-<form action='add.php' method='POST' class="form" id="form1">
+<form class="form" id="form">
+
+<div class="sub">
+
+<h2 align="center">新規登録</h2>
+</div>
+
 <p class="p_name">
 <input type="text" name="p_name" id="p_name"
 class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="親御さんのお名前"
 id="p_name"　size=20
-value="<?php echo $_SESSION['arr'][1];?>" onkeyup="visible();"
+value="<?php echo $_SESSION['arr'][2];?>" onkeyup="visible();"
 onchange="visible();" />
 </p>
 
@@ -62,25 +67,19 @@ onchange="visible();" />
 <input type="text" name="c_name" id="c_name"
 class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="お子さんの名前"
 id="c_name"　size=20
-value="<?php echo $_SESSION['arr'][2];?>" onkeyup="visible();"
+value="<?php echo $_SESSION['arr'][3];?>" onkeyup="visible();"
 onchange="visible();" />
 </p>
 <p class="email">
 <input type="text" name="email" id="email"
 class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="メールアドレス"
 id="email"　size=20
-value="<?php echo $_SESSION['arr'][3];?>" onkeyup="visible();"
+value="<?php echo $_SESSION['arr'][1];?>" onkeyup="visible();"
 onchange="visible();" />
 </p>
-    
-<input type="text" name="zip11" id="zip11" size="10" maxlength="8" value="<?php echo $_SESSION['arr'][5];?>" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11'); "onchange="visible();" >
-
-住所
-						<input type="text" name="addr11" id="addr11" size=60
-							value="<?php echo $_SESSION['arr'][6];?>" onkeyup="visible();"
-							onchange="visible();" />
-
-
+<div class="style-select slate">
+						<th align="left">
+                        <div id="mainselection1">
                         <select name = "graduation_year"  onchange = "visibleByselect();">
                         <option value = "" selected>卒業年度</option>
                         <option value = "2016">2016年度</option>
@@ -94,22 +93,31 @@ onchange="visible();" />
                         <option value = "2008">2008年度</option>
                         <option value = "2007">2007年度</option>
                         </select>
+                        </div>
+						</th>
+					</tr>
 
-
-
+                        <th align="left">
+                        <div id="mainselection2">
                         <select name = "issend"  onchange = "visibleByselect();">
                         <option value = "" selected>配信希望</option>
                         <option value = "1">許可</option>
                         <option value = "0">不可</option>
 
                     </select>
-<font face="Hiragino Maru Gothic ProN">
+                    </div>
+                    </th>
+
+</div>
+
 </font>
 <div id = "foot">
-<input type="submit" name="add" id="add" value="追加する" style=" margin: 10px;" disabled />
-<input type="reset" value ="リセット" style=" margin: 10px;" />
-<input type="button" value="戻る" onclick="tolistback()" style=" margin: 10px;">
+<input type="submit" name="add" id="add" value="追加する" style=" margin: 9px;" disabled />
+<input type="reset" value ="リセット" style=" margin: 9px;" />
+<input type="button" value="戻る" onclick="tolistback()" style=" margin: 9px;">
 </div>
+
+</form>
 </form>
 
 </div>
--- confirm.php
+++ confirm.php
@@ -12,6 +12,7 @@ session_start();
                  $query = "INSERT INTO `member` (`email`, `p_name`, `c_name`, `graduation_year`, `issend`)" . "VALUES (:email, :p_name, :c_name, :graduation_year, :issend);";
                 
                     echo $email;
+                    echo $issend_flag;
 					$stmt = $pdo -> prepare($query);
 					$stmt -> bindParam(':email', $email, PDO::PARAM_STR);
 					$stmt -> bindParam(':p_name', $p_name, PDO::PARAM_STR);
@@ -24,6 +25,8 @@ session_start();
 				echo "ERROR:" . $e->getMessage();
 			}
 		}
+    if($issend == 1)  $issend_flag = "許可";
+    else  $issend_flag = "不可";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -31,30 +34,69 @@ session_start();
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>イベント管理システム-確認-</title>
-<link rel="STYLESHEET" href="./css/add.css" type="text/css">
+<link rel="STYLESHEET" href="./css/confirm.css" type="text/css">
 </head>
 <body>
-	<div id=site-box>
-		<div id=head>
-			<h2>登録内容を確認してください☆</h2>
+	<div id="site-box">
+		<div id="head">
+
 		</div>
-		<div id=main>
-           <?php 
-             echo "<table>"; 
-             echo "<tr><th>氏名</th><th>".$p_name."</th></tr>";
-             echo "<tr><th>お子さんの氏名</th><th>".$c_name."</th></tr>"; 
-             echo "<tr><th>メールアドレス</th><th>".$email."</th></tr>"; 
-             echo "<tr><th>卒業年度</th><th>".$graduation_year."年度</th></tr>"; 
-             if($issend == 1) echo "<tr><th>配信希望</th><th>あり</th></tr>";
-             else echo "<tr><th>配信希望</th><th>".$p_name."</th></tr>"; 
-             echo "</table>";
-            ?>
-			<form action='confirm.php' method='POST'>
-				<input type="submit" name="confirm" id="confirm" value="完了" style="margin: 10px; float: right;" />
+            <div id="cform-main">
+            <div id="cform-div">
+            <form class="cform" id="cform">
+
+<div class="sub">
+
+<h2 align="center">登録内容を確認してください⭐︎</h2>
+</div>
+<p class="cp_name">
+<input type="text" name="cp_name" id="cp_name"
+class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder=$p_name
+id="cp_name"　size=20
+value="<?php echo $p_name;?>" onkeyup="visible();"
+onchange="visible();" readonly/>
+</p>
+
+<p class="cc_name">
+<input type="text" name="cc_name" id="cc_name"
+class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="お子さんの名前"
+id="cc_name"　size=20
+value="<?php echo $c_name;?>" onkeyup="visible();"
+onchange="visible();" readonly/>
+</p>
+
+<p class="cemail">
+<input type="text" name="cemail" id="cemail"
+class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="メールアドレス"
+id="cemail"　size=20
+value="<?php echo $email;?>" onkeyup="visible();"
+onchange="visible();" readonly/>
+</p>
+
+<p class="graduate">
+<input type="text" name="graduate" id="graduate"
+class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="お子さんの名前"
+id="graduate"　size=20
+value="<?php echo $graduation_year;?>" onkeyup="visible();"
+onchange="visible();" readonly/>
+</p>
+
+<p class="deli">
+<input type="text" name="deli" id="deli"
+class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="メールアドレス"
+id="deli"　size=20
+value="<?php echo $issend_flag;?>" onkeyup="visible();"
+onchange="visible();" readonly/>
+
+</p>
+ </form>
+</form>			<form action='confirm.php' method='POST'>
+				<input type="submit" name="confirm" id="confirm" value="完了"  />
 			</form>
             <form action='add.php' method='POST'>
-				<input type="submit" name="revise" id="revise" value="修正" style="margin: 10px; float: right;" />
+				<input type="submit" name="revise" id="revise" value="修正"  />
 			</form>
+
 		</div>
 	</div>
 </body>
